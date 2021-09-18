460b4086c8c66d3b5d762cc7303ab1a9f9285cc6
kametan0730 <kametan0730@gmail.com>
2021-09-19 05:02:32 +0900

    プレフィックス長を指定してサーチできるように

--- main.cpp
+++ main.cpp
@@ -23,21 +23,26 @@ int main(){
     add_prefix(root, ntohl(inet_addr("10.20.31.0")), 24, inet_addr("10.20.31.1"));
     add_prefix(root, ntohl(inet_addr("10.20.40.0")), 24, inet_addr("10.20.40.1"));
 
-    node* n1 = add_prefix(root, ntohl(inet_addr("1.0.0.0")), 4, inet_addr("10.0.0.1"));
-    node* n2 = add_prefix(root, ntohl(inet_addr("1.0.0.0")), 8, inet_addr("10.0.0.1"));
+    //node* n1 = add_prefix(root, ntohl(inet_addr("1.0.0.0")), 4, inet_addr("10.0.0.1"));
+    //node* n2 = add_prefix(root, ntohl(inet_addr("1.0.0.0")), 8, inet_addr("10.0.0.1"));
 
-    // assert_tree(root);
-
-
-    node* res = search_prefix(root, ntohl(inet_addr("10.20.30.1")));
-    uint8_t prefix_len = res->prefix_len;
-
-    printf("%d\n", prefix_len);
-    printf("%s\n", inet_ntoa(in_addr{.s_addr = res->next_hop}));
-
-    delete_prefix(n1);
+    assert_tree(root);
 
 
+    node* res = search_prefix(root, ntohl(inet_addr("10.20.30.1")), 25, true);
+
+    if(res == nullptr){
+        printf("Result is null\n");
+    }else{
+        uint8_t prefix_len = res->prefix_len;
+        printf("%d\n", prefix_len);
+        printf("%s\n", inet_ntoa(in_addr{.s_addr = res->next_hop}));
+    }
+
+    //delete_prefix(n1);
+
+
+/*
 
     add_prefix(root, ntohl(inet_addr("103.247.181.0")), 24, inet_addr("103.247.181.1"));
     add_prefix(root, ntohl(inet_addr("103.247.181.224")), 28, inet_addr("103.247.181.224"));
@@ -49,13 +54,19 @@ int main(){
     //print_address_binary_2(inet_addr("103.247.181.225"));
 
     node* res2 = search_prefix(root, ntohl(inet_addr("103.247.181.227")));
-    uint8_t prefix_len2 = res2->prefix_len;
+    if(res2 == nullptr){
+        printf("Result is null\n");
 
-    printf("%d\n", prefix_len2);
-    printf("%s\n", inet_ntoa(in_addr{.s_addr = res2->next_hop}));
+    }else{
+        uint8_t prefix_len2 = res2->prefix_len;
+
+        printf("%d\n", prefix_len2);
+        printf("%s\n", inet_ntoa(in_addr{.s_addr = res2->next_hop}));
+
+    }
 
 
-
+ */
     //delete_prefix(root, true);
 
     //assert_tree(root);
