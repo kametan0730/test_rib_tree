e4e0faec10e01e82818167b947d268498e37856e
kametan0730 <kametan0730@gmail.com>
2021-09-23 17:37:34 +0900

    すべてヘッダファイルにまとめる

--- tree.h
+++ tree.h
@@ -30,8 +30,20 @@ struct node{
     node* node_1 = nullptr;
 };
 
-bool check_bit(uint32_t addr, uint8_t n);
-void print_address_binary(uint32_t addr);
+bool check_bit(uint32_t addr, uint8_t n){
+    return (addr >> (31 - n)) & 0b01;
+}
+
+void print_address_binary(uint32_t addr){
+    for(int i = 31; i >= 0; --i){
+        if(check_bit(addr, i)){
+            printf("1");
+        }else{
+            printf("0");
+        }
+    }
+    printf("\n");
+}
 
 template <typename DATA_TYPE>
 void assert_tree(node<DATA_TYPE>* node){
